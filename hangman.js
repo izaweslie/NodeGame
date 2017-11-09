45f5fcc68c30f9b5d2ff8f38ac98484c2e18b442
izaweslie <izaweslie@gmail.com>
2017-11-08 18:40:23 -0600

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,6 +1,6 @@
 //Require module dependencies
 var inquirer = require('inquirer');
-var Game = require('.game.js');
+var Game = require('./game.js');
 
 //Initialize Game module with a new game
 Game = new Game();
